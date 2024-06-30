8c6234253dd448e07a1fa2be5a8f20b9e754b66b
Alinaga1 <139326824+Alinaga1@users.noreply.github.com>
2024-06-30 23:13:57 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const mainImage = document.getElementById('main-image');
     const thumbnails = document.querySelectorAll('.thumbnail');
     let currentHiddenIndices = new Array(thumbnails.length).fill(0);
+    let isTouchDevice = 'ontouchstart' in document.documentElement;
 
     thumbnails.forEach((thumbnail, index) => {
         const thumbnailImage = thumbnail.querySelector('img');
@@ -12,18 +13,42 @@ document.addEventListener("DOMContentLoaded", function() {
             mainImage.src = thumbnailImage.src;
         });
 
-        // Прокрутка колесика мыши
-        thumbnail.addEventListener('wheel', function(event) {
-            event.preventDefault();
-            if (event.deltaY < 0) {
-                // Прокрутка вверх
-                currentHiddenIndices[index] = (currentHiddenIndices[index] - 1 + hiddenImages.length) % hiddenImages.length;
-            } else {
-                // Прокрутка вниз
-                currentHiddenIndices[index] = (currentHiddenIndices[index] + 1) % hiddenImages.length;
-            }
-            thumbnailImage.src = hiddenImages[currentHiddenIndices[index]].src;
-        });
+        if (isTouchDevice) {
+            // Свайпы для мобильных устройств
+            let startX;
+
+            thumbnail.addEventListener('touchstart', function(event) {
+                startX = event.touches[0].clientX;
+            });
+
+            thumbnail.addEventListener('touchmove', function(event) {
+                event.preventDefault();
+                let endX = event.touches[0].clientX;
+
+                if (startX - endX > 50) {
+                    // Свайп влево
+                    currentHiddenIndices[index] = (currentHiddenIndices[index] + 1) % hiddenImages.length;
+                    thumbnailImage.src = hiddenImages[currentHiddenIndices[index]].src;
+                } else if (endX - startX > 50) {
+                    // Свайп вправо
+                    currentHiddenIndices[index] = (currentHiddenIndices[index] - 1 + hiddenImages.length) % hiddenImages.length;
+                    thumbnailImage.src = hiddenImages[currentHiddenIndices[index]].src;
+                }
+            });
+        } else {
+            // Прокрутка колесика мыши для десктопов
+            thumbnail.addEventListener('wheel', function(event) {
+                event.preventDefault();
+                if (event.deltaY < 0) {
+                    // Прокрутка вверх
+                    currentHiddenIndices[index] = (currentHiddenIndices[index] - 1 + hiddenImages.length) % hiddenImages.length;
+                } else {
+                    // Прокрутка вниз
+                    currentHiddenIndices[index] = (currentHiddenIndices[index] + 1) % hiddenImages.length;
+                }
+                thumbnailImage.src = hiddenImages[currentHiddenIndices[index]].src;
+            });
+        }
 
         // Клик по скрытым изображениям
         hiddenImages.forEach(hiddenImage => {
